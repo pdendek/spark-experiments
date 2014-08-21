d47cea21056f7fbb1715f3253de9fa5e1ac99650
pdendek <pdendek@icm.edu.pl>
2014-08-21 18:20:52 +0200

    finally obtained working version

--- submit.sh
+++ submit.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
 # Example usage:
-# ./submit.sh target/spark-intro-1.0-SNAPSHOT-jar-with-dependencies.jar -Dspark.master=local SimpleApp
+# ./submit.sh SimpleApp target/spark-intro-1.0-SNAPSHOT-jar-with-dependencies.jar 
+CLASS=$1
+JAR_FILE=$2
+REMINDER=${@:3}
 
-JAR_FILE=$1
-REMINDER=${@:2}
-
-source /etc/spark/conf/spark-env.sh
-
-SPARK_HOME=/usr/lib/spark; export SPARK_HOME
-HADOOP_HOME=/usr/lib/hadoop; export HADOOP_HOME
-
-# system jars:
-CLASSPATH=/etc/hadoop/conf
-CLASSPATH=$CLASSPATH:$HADOOP_HOME/*:$HADOOP_HOME/lib/*
-CLASSPATH=$CLASSPATH:$HADOOP_HOME/../hadoop-mapreduce/*:$HADOOP_HOME/../hadoop-mapreduce/lib/*
-CLASSPATH=$CLASSPATH:$HADOOP_HOME/../hadoop-yarn/*:$HADOOP_HOME/../hadoop-yarn/lib/*
-CLASSPATH=$CLASSPATH:$HADOOP_HOME/../hadoop-hdfs/*:$HADOOP_HOME/../hadoop-hdfs/lib/*
-CLASSPATH=$CLASSPATH:$SPARK_HOME/assembly/lib/*
-
-# app jar:
-CLASSPATH=$CLASSPATH:"$JAR_FILE"
-CONFIG_OPTS="-Dspark.jars=$JAR_FILE"
-java -cp $CLASSPATH $CONFIG_OPTS $REMINDER
+spark-submit --class ${CLASS} --master local ${JAR_FILE} ${REMINDER} 
