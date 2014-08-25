f320b08d113f426c39796f934eedde95264b7225
pdendek <pdendek@icm.edu.pl>
2014-08-25 14:23:14 +0200

    minor changes in pom.xml; spark job submission is now done without need of passing jar-with-dependencies

--- submit.sh
+++ submit.sh
@@ -6,4 +6,22 @@ CLASS=$1
 JAR_FILE=$2
 REMINDER=${@:3}
 
-spark-submit --class ${CLASS} --master local ${JAR_FILE} ${REMINDER} 
+CLASSPAT1=$CLASSPAT1,$HADOOP_HOME/*:$HADOOP_HOME/lib/*
+CLASSPAT1=$CLASSPAT1,$HADOOP_HOME/../hadoop-mapreduce/*,$HADOOP_HOME/../hadoop-mapreduce/lib/*
+CLASSPAT1=$CLASSPAT1,$HADOOP_HOME/../hadoop-yarn/*,$HADOOP_HOME/../hadoop-yarn/lib/*
+CLASSPAT1=$CLASSPAT1,$HADOOP_HOME/../hadoop-hdfs/*,$HADOOP_HOME/../hadoop-hdfs/lib/*
+CLASSPAT1=$CLASSPAT1,$SPARK_HOME/assembly/lib/*
+
+CLASSPATH=""
+for i in $HADOOP_HOME $HADOOP_HOME/../hadoop-mapreduce $HADOOP_HOME/../hadoop-yarn $HADOOP_HOME/../hadoop-hdfs $SPARK_HOME/assembly/lib ;
+do
+ CLASSPATH="`ls -1 ${i}/*.jar | tr "\\n" ","`${CLASSPATH}"
+done;
+
+
+CLASSPATH=`echo $CLASSPATH | rev | cut -c2- | rev`
+
+#echo $CLASSPATH
+#echo $CLASSPAT1
+
+spark-submit --class ${CLASS}  --jars ${CLASSPATH} --master local ${JAR_FILE} ${REMINDER} 
